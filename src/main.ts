604d04a49ea0b48215c4a1bf0575ac806ed90407
Noyan <noyan.alimov@gmail.com>
2021-03-06 12:34:41 +0000

    configured cors

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-    );
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET, POST, PUT, PATCH, DELETE, OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   });
 
   app.setGlobalPrefix('api/v0/main-backend');
