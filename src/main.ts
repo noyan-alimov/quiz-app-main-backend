d1a3610fe9dbb7af2fee895d84966eb928846ea7
Noyan <noyan.alimov@gmail.com>
2021-03-06 12:29:25 +0000

    configured cors

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,11 @@ async function bootstrap() {
 
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
 
