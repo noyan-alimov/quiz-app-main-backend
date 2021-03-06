9d4cf61088297642705a4150d81d47a43a59b9bd
Noyan <noyan.alimov@gmail.com>
2021-03-06 11:54:53 +0000

    changed cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://msa-landing-page.web.app/'
+    origin: '*'
   });
   app.setGlobalPrefix('api/v0/main-backend');
 
